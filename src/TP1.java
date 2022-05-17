c1a59a27f80bfa23b64a7deeb8db193c5fc1e8fe
Thomas-D40 <alexander.darracq@gmail.com>
2022-05-17 10:39:44 +0200

    TP1 fait

--- src/TP1.java
+++ src/TP1.java
@@ -1,2 +1,35 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class TP1 {
+
+
+
+    public static void calculate() {
+        Scanner scanner = new Scanner(System.in);
+
+        float diametre = 0;
+
+
+        while (diametre == 0) {
+            try {
+                System.out.println("Quelle est le diametre de votre surface?");
+                diametre = scanner.nextFloat();
+            } catch (InputMismatchException e) {
+                System.out.println("Vous n'avez pas rentré de chiffre");
+                scanner.nextLine();
+            }
+        }
+        ;
+
+        float rayon = diametre / 2;
+        float surface = (float) (rayon * rayon * Math.PI);
+
+        System.out.println("Surface : " + surface);
+        System.out.println("Rayon : " + rayon);
+        scanner.close();
+    }
+
+
+
 }
