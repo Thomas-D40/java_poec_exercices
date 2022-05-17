68493a9e227cf096cf3b1010d99b11184fe5b318
Thomas-D40 <alexander.darracq@gmail.com>
2022-05-17 12:31:29 +0200

    ÃCorrection exercice

---
+++ src/TP4.java
@@ -0,0 +1,34 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class TP4 {
+
+    public static void typeOfNumber() {
+        Scanner scan = new Scanner(System.in);
+        Integer number = null;
+        
+
+        while (number == null) {
+            try {
+                System.out.println("Entrez un nombre entier");
+                number = scan.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Vous n'avez pas mis un nombre entier");
+                scan.nextLine();
+            }
+        }
+    
+        boolean isEven = number%2 == 0;
+
+        if (number >= 0) {
+            System.out.printf(number + " est positif et %s", (isEven?"pair":"impair"));
+        }
+        if (number < 0) {
+            System.out.printf(number + " est négatif et %s", (isEven?"pair":"impair"));
+        }
+
+        scan.close();
+
+
+    }
+}
